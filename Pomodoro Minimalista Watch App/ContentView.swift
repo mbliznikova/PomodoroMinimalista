5166ccfd8b32b582b09053b01030ee60fc24cbf5
Margarita Bliznikova <captainderteufel@gmail.com>
2025-06-20 11:17:54 -0700

    Add a circle for a progress bar

--- Pomodoro Minimalista Watch App/ContentView.swift
+++ Pomodoro Minimalista Watch App/ContentView.swift
@@ -11,15 +11,20 @@ struct ContentView: View {
     @State private var isTimerRunning: Bool = false
     
     var body: some View {
-        VStack {
+        ZStack {
+
+            Circle()
+                .stroke(lineWidth: 5)
+                .foregroundColor(.red)
+                .frame(width: 200, height: 200)
+
             Button("", systemImage: isTimerRunning ? "stop.fill" : "play.fill" ) {
                 isTimerRunning.toggle()
             }
             .buttonStyle(PlainButtonStyle())
-            .font(.system(size: 40))
+            .font(.system(size: 70))
             .foregroundStyle(.red)
         }
-        .padding()
     }
 }
 
