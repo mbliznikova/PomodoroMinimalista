ada55b4bb1631cc9f54d257972d92ba946c6cba2
Margarita Bliznikova <captainderteufel@gmail.com>
2026-01-19 17:34:58 -0800

    Update styling in StatsView

--- Pomodoro Minimalista Watch App/StatsView.swift
+++ Pomodoro Minimalista Watch App/StatsView.swift
@@ -14,22 +14,26 @@ struct StatsView: View {
     @ObservedObject var timerController: TimerController
 
     var body: some View {
-        VStack {
-            Text("Today")
-                .foregroundColor(.red)
-                .font(.headline)
-            Text("\(Int(timerController.dailySessionCount))")
-                .foregroundColor(.red)
-                .padding()
-                .font(.title)
-            Text("Total")
-                .padding()
-                .foregroundColor(.red)
-                .font(.headline)
-            Text("\(Int(timerController.totalSessionCount))")
-                .foregroundColor(.red)
-                .padding()
-                .font(.title)
+        VStack(spacing: 10) {
+            Spacer()
+            VStack {
+                Text("\(Int(timerController.dailySessionCount))")
+                    .foregroundColor(.red)
+                    .font(.system(size: 48))
+                Text("Today")
+                    .foregroundColor(.red)
+                    .font(.caption)
+
+            }
+            VStack {
+                Text("\(Int(timerController.totalSessionCount))")
+                    .font(.system(size: 48))
+                    .foregroundColor(.red)
+                Text("Total")
+                    .foregroundColor(.red)
+                    .font(.caption)
+            }
+            Spacer()
         }
     }
 }
