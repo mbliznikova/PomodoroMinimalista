ffe81710d0312227ac7260063a20f7acd11aa014
Margarita Bliznikova <captainderteufel@gmail.com>
2026-01-22 18:38:14 -0800

    Add placeholders to the iOS app

--- Pomodoro Minimalista/ContentView.swift
+++ Pomodoro Minimalista/ContentView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pickerValue: Double = 25.0
+
     var body: some View {
         VStack {
-            Text("Thank you for using the app!")
-            Text("More updates coming soon!")
+
+            VStack {
+                Text("Session duration")
+                    .foregroundColor(.red)
+                Picker("Session duration", selection: $pickerValue) {
+                    ForEach(1...60, id: \.self) { number in
+                        Text("\(Int(number))")
+                            .font(.headline)
+                            .foregroundColor(.red)
+                    }
+                }
+                .pickerStyle(.wheel)
+            }
+
+            ZStack {
+                Circle()
+                    .stroke(
+                        Color.red,
+                        style: StrokeStyle(
+                            lineCap: .round
+                        ))
+                    .rotationEffect(.degrees(-90))
+                    ._statusBarHidden()
+
+            }
+
+            VStack{
+                Text("Today")
+                    .font(.headline)
+                    .foregroundColor(.red)
+                Text("Total")
+                    .font(.headline)
+                    .foregroundColor(.red)
+            }
+            .padding()
         }
         .padding()
     }
